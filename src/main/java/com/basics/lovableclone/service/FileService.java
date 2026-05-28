18bfd2e8b5227109117d8f67685f185bd47ab98e
Viraj <vrjraj508@gmail.com>
2026-05-28 18:39:54 +0530

    28th May New topic

--- src/main/java/com/basics/lovableclone/service/FileService.java
+++ src/main/java/com/basics/lovableclone/service/FileService.java
@@ -1,5 +1,6 @@
 package com.basics.lovableclone.service;
 
+import com.basics.lovableclone.dtos.project.FileContentResponse;
 import com.basics.lovableclone.dtos.project.FileNode;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +9,6 @@ import java.util.List;
 @Service
 public interface FileService {
     List<FileNode> getFileTree(Long projectId);
+
+    FileContentResponse getFileContent(Long projectId, String path, Long userid);
 }
